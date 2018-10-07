37de2e0e1d2d251cd82f6f1aebbc47d2fcfdacb6
lopezoliveraeddy <lopezoliveraeddy@gmail.com>
2018-10-07 03:58:04 +0000

    first commit

---
+++ application/views/partial/footer.php
@@ -0,0 +1,10 @@
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="jumbotron push-spaces">
+			<strong><?php echo $this->lang->line('common_you_are_using_ospos'); ?>
+		</div>
+	</div>
+</body>
+</html>
